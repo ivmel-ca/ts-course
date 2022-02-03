35a0a0e1551d3156f6650aece5554f7e0af81e60
Ivan Melnychenko <ivmel.ca@gmail.com>
2022-02-03 14:31:29 +0200

    feat: type annotations and type inference

---
+++ features/annotations/functions.ts
@@ -0,0 +1,51 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// before ES2015
+const weatherToday = {
+  date: new Date(),
+  weather: "sunny"
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(weatherToday);
+
+//after ES2015
+
+const getLoggedWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+getLoggedWeather(weatherToday);
