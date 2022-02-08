2aed335e414bc76dc2bc6210ede0c1f5c486cf12
Ivan Melnychenko <ivmel.ca@gmail.com>
2022-02-08 13:25:51 +0200

    feat: add inheritance and composition approaches to create generic classes

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,25 @@
-import fs from "fs";
+import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+// Create an obj that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader("football.csv");
 
-console.log(matches);
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
+
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
