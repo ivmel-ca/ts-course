f2abb1f246788d6fc4a7723b9fffb9746e815dee
Ivan Melnychenko <ivmel.ca@gmail.com>
2022-02-14 17:57:30 +0200

    feat: setup a framework project. Add User class, add db mock

---
+++ web/src/models/User.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosResponse } from "axios";
+
+interface UserProps {
+  name?: string;
+  age?: number;
+  id?: number;
+}
+
+//type alias
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName] || [];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((resp: AxiosResponse): void => {
+        this.set(resp.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios
+        .put(`http://localhost:3000/users/${id}`, this.data)
+        .then((resp: AxiosResponse): void => {
+          this.set(resp.data);
+        });
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
+  }
+}
